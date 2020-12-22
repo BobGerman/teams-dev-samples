e7b59c82ed75d819531bddfe2c7041e42a145f9d
Bob German <bgerman1@hotmail.com>
2020-12-22 15:43:02 -0500

    Theme changes work

--- samples/tab-create-react-app-ts/src/components/TabPage.tsx
+++ samples/tab-create-react-app-ts/src/components/TabPage.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import { IConfigInfo, ConfigService } from '../services/ConfigService';
 import * as microsoftTeams from "@microsoft/teams-js";
+import { Provider, Header, ThemePrepared } from "@fluentui/react-northstar";
+import { teamsTheme, teamsDarkTheme, teamsHighContrastTheme } from '@fluentui/react-northstar';
 
 /**
  * The web UI to display in the Teams UI
  */
-export interface ITabPageProps{ }
+export interface ITabPageProps { }
 export interface ITabPageState {
   config?: IConfigInfo;
+  theme: ThemePrepared;
 }
 
 export default class WebPage extends React.Component<ITabPageProps, ITabPageState> {
 
-  constructor (props: ITabPageProps) {
+  constructor(props: ITabPageProps) {
     super(props);
     this.state = {
-      config: undefined
+      config: undefined,
+      theme: teamsTheme
     }
   }
 
-  async componentDidMount () {
+  async componentDidMount() {
     let configInfo = await ConfigService.getConfigInfo();
     this.setState({
-      config: configInfo
+      config: configInfo,
+      theme: this.getTheme(configInfo.teamsContext?.theme)
+    });
+    microsoftTeams.registerOnThemeChangeHandler((theme: string = "default"): void => {
+
+      this.setState({
+        theme: this.getTheme(theme)
+      });
     });
     microsoftTeams.appInitialization.notifyAppLoaded();
+  }
+
+  private getTheme(theme?: string): ThemePrepared {
+    let result = teamsTheme;
+    if (theme === 'dark') result = teamsDarkTheme;
+    if (theme === 'contrast') result = teamsHighContrastTheme
+    return result
   }
 
   render() {
@@ -32,9 +50,10 @@ export default class WebPage extends React.Component<ITabPageProps, ITabPageStat
     if (!this.state.config?.teamsContext) {
       return <div>loading...</div>
     } else {
+
       return (
-        <div>
-          <h1>{process.env.REACT_APP_MANIFEST_NAME}</h1>
+        <Provider theme={this.state.theme}>
+          <Header>{process.env.REACT_APP_MANIFEST_NAME}</Header>
           <p>{this.state.config.teamsContext.teamName ?
             `You are in ${this.state.config.teamsContext.teamName}` :
             `You are not in a Team`
@@ -42,9 +61,9 @@ export default class WebPage extends React.Component<ITabPageProps, ITabPageStat
           <p>Your app is running in the Teams UI</p>
           <p>Your short message is {this.state.config.shortMessage}</p>
           <p></p>
-        </div>
+        </Provider>
       );
-      }
+    }
   }
 
 }
